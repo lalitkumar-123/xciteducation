c61eccd69e475e9ac0b7fe25102238ba36d2bfcb
BristiJana <54547239+BristiJana@users.noreply.github.com>
2021-09-04 01:33:45 +0530

    Update service page

--- src/Pages/Service.js
+++ src/Pages/Service.js
@@ -347,11 +347,11 @@ function Service() {
                     </div>
                   </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" ></span>
+                <button class="carousel-control-prev noHover" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon " ></span>
                   <span class="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <button class="carousel-control-next noHover" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
                   <span class="carousel-control-next-icon"></span>
                   <span class="visually-hidden">Next</span>
                 </button>
